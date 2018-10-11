7ae604a09106fc925de8ef49914e1d1c722ea6de
Cedulio Cezar <ceduliocezar@gmail.com>
2018-10-11 10:04:37 -0300

    Add session stats

--- README.md
+++ README.md
@@ -16,16 +16,17 @@ This application can be accessed on [http://www.shortcutlab.space/](http://www.s
 
 ## Create your own lab
 
-Even tough shortculab has a list of pre defined labs the chances are that we don't support your application so far. 
+Even tough shortculab has a list of pre defined labs the chances are that we don't support your application so far.
 
 With this in mind you can create your own lab that meets your specific needs using the [sample lab](labs/sample_1.0.0_all.lab) as example and use it during your training session.
 
 Even better if you submit a pull request introducing your newly created lab of your awesome application.
 
 ## Built With
-* Javascript - No dependencies so far
-* CSS3 - No dependencies so far
-* HTML5
+
+- Javascript - No dependencies so far
+- CSS3 - No dependencies so far
+- HTML5
 
 ## Contributing
 
@@ -33,7 +34,7 @@ Contributions are welcome and encouraged.
 
 ## Authors
 
-* **Cedulio Cezar** - *Initial work* - [Github](https://github.com/ceduliocezar)
+- **Cedulio Cezar** - _Initial work_ - [Github](https://github.com/ceduliocezar)
 
 See also the list of [contributors](https://github.com/ceduliocezar/shortcutlab/graphs/contributors) who participated in this project.
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,196 +1,318 @@
+const labsAPIURL = "https://api.github.com/repos/ceduliocezar/shortcutlab/contents/labs/";
+
 var keysPressed = [];
-var current = 0;
 
-window.addEventListener("keydown", function (event) {
-    const key = event.key;
-    if (!keysPressed.includes(key)) {
-        console.log("New key down identified:");
-        keysPressed.push(key);
-        printKeysPressedState();
-    }
-    event.preventDefault();
-
-}, false);
-
-window.addEventListener("keyup", function (event) {
-    if (keysPressed.length > 0) {
-        matchCombination(keysPressed);
-        keysPressed = [];
-        printKeysPressedState();
-        displayNextShortcut();
-    }
-    event.preventDefault();
-});
+var selectedLab;
+var currentShortcutIndex;
+var loadedLabs;
+var totalCorrectAnswers = 0;
 
 redirectToHTTPSIfNecessary();
+clearKeysPressedWhenEnterOnBackgroundState();
 
 function redirectToHTTPSIfNecessary() {
-    var host = "shortcutlab.space";
-    if ((window.location.host.includes(host)) && (window.location.protocol != "https:")) {
-        window.location.protocol = "https";
-    }
+  var host = "shortcutlab.space";
+  if (window.location.host.includes(host) && window.location.protocol != "https:") {
+    window.location.protocol = "https";
+  }
 }
 
-
 function displayNextShortcut() {
-    console.log(`displayNextShortcut: current index ${currentShortcutIndex}`);
-    currentShortcutIndex = currentShortcutIndex + 1;
+  console.log(`displayNextShortcut: current index ${currentShortcutIndex}`);
+  currentShortcutIndex = currentShortcutIndex + 1;
+
+  if (currentShortcutIndex < selectedLab.shortcuts.length) {
     displayShortcut(selectedLab.shortcuts[currentShortcutIndex]);
+  } else {
+    endLabSession();
+  }
 }
 
 function displayShortcut(shortcut) {
-    console.log(`displayShortcut: shortcut ${JSON.stringify(shortcut)}`)
-    var description = shortcut.description;
-    var category = shortcut.category === undefined ? 'General' : shortcut.category;
-    var labInfo = `${selectedLab.software} - ${selectedLab.version} - ${selectedLab.platform}`;
+  console.log(`displayShortcut: shortcut ${JSON.stringify(shortcut)}`);
+  var description = shortcut.description;
+  var category =
+    shortcut.category === undefined ? "General" : shortcut.category;
+  var labInfo = `${selectedLab.software} - ${selectedLab.version} - ${selectedLab.platform}`;
 
-    document.getElementsByClassName("shortcut-description")[0].innerHTML = description;
-    document.getElementsByClassName("shortcut-category")[0].innerHTML = category;
-    document.getElementsByClassName("lab-info")[0].innerHTML = labInfo;
+  document.getElementsByClassName("shortcut-description")[0].innerHTML = description;
+  document.getElementsByClassName("shortcut-category")[0].innerHTML = category;
+
+  var elements = document.getElementsByClassName("lab-info");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = labInfo;
+  }
+
 }
 
 function printKeysPressedState() {
-    console.log("printKeysPressedState: " + keysPressed)
-    document.getElementsByClassName("shortcut-input")[0].value = keysPressed.join(' + ');
+  console.log("printKeysPressedState: " + keysPressed);
+  document.getElementsByClassName("shortcut-input")[0].value = keysPressed.join(" + ");
 }
 
-function matchCombination(keyCombination) {
-    var currentShortcut = selectedLab.shortcuts[currentShortcutIndex]
+function matchCombination() {
+  var currentShortcut = selectedLab.shortcuts[currentShortcutIndex];
 
-    if (keyCombination.join(", ").toLowerCase() === currentShortcut.keys.join(", ").toLowerCase()) {
-        displayAsCorrect();
-    } else {
-        displayAsIncorrect();
-    }
+  if (keysPressed.join(", ").toLowerCase() === currentShortcut.keys.join(", ").toLowerCase()) {
+    displayAsCorrect();
+    totalCorrectAnswers++;
+  } else {
+    displayAsIncorrect();
+  }
+}
+
+function endLabSession() {
+  console.log('endLabSession');
+  unregisterKeyListeners();
+  displayEndSessionView();
+  document.getElementsByClassName("correct-score")[0].innerHTML = totalCorrectAnswers;
+  document.getElementsByClassName("total-shortcuts")[0].innerHTML = selectedLab.shortcuts.length;
+}
+
+function displayEndSessionView() {
+  console.log('displayEndSessionView');
+  document.getElementsByClassName("lab-session-container")[0].style.display = "none";
+  document.getElementsByClassName("end-session-container")[0].style.display = "block";
 }
 
 function displayAsCorrect() {
-    let li = document.createElement("li");
-    li.classList.add('correct');
-    li.classList.add('shortcut-item');
-    li.appendChild(createHistoryDescriptionItem());
-    li.appendChild(createHistoryCombinationItem());
-    document.getElementsByClassName("shortcut-list")[0].prepend(li);
+  let li = document.createElement("li");
+  li.classList.add("correct");
+  li.classList.add("shortcut-item");
+  li.appendChild(createHistoryDescriptionItem());
+  li.appendChild(createHistoryCombinationItem());
+  document.getElementsByClassName("shortcut-list")[0].prepend(li);
 }
 
 function displayAsIncorrect() {
-    let li = document.createElement("li");
-    li.classList.add('incorrect');
-    li.classList.add('shortcut-item');
-    li.appendChild(createHistoryDescriptionItem());
-    li.appendChild(createHistoryCombinationItem());
-    document.getElementsByClassName("shortcut-list")[0].prepend(li);
+  let li = document.createElement("li");
+  li.classList.add("incorrect");
+  li.classList.add("shortcut-item");
+  li.appendChild(createHistoryDescriptionItem());
+  li.appendChild(createHistoryCombinationItem());
+  document.getElementsByClassName("shortcut-list")[0].prepend(li);
 }
 
 function createHistoryDescriptionItem() {
-    var currentShortcut = selectedLab.shortcuts[currentShortcutIndex];
+  var currentShortcut = selectedLab.shortcuts[currentShortcutIndex];
 
-    let descriptionElement = document.createElement("p");
-    descriptionElement.innerHTML = currentShortcut.description;
-    descriptionElement.classList.add('history-item-description');
+  let descriptionElement = document.createElement("p");
+  descriptionElement.innerHTML = currentShortcut.description;
+  descriptionElement.classList.add("history-item-description");
 
-    return descriptionElement;
+  return descriptionElement;
 }
 function createHistoryCombinationItem() {
-    var currentShortcut = selectedLab.shortcuts[currentShortcutIndex];
-    let combinationElement = document.createElement("p");
-    combinationElement.innerHTML = currentShortcut.keys.join(' + ');
-    combinationElement.classList.add('history-item-combination');
+  var currentShortcut = selectedLab.shortcuts[currentShortcutIndex];
+  let combinationElement = document.createElement("p");
+  combinationElement.innerHTML = currentShortcut.keys.join(" + ");
+  combinationElement.classList.add("history-item-combination");
 
-    return combinationElement;
+  return combinationElement;
 }
 
 function displaySelectionView() {
-    console.log('displaySelectionView: ');
-    document.getElementsByClassName("intro-container")[0].style.display = "none";
-    document.getElementsByClassName("content")[0].style.display = "block";
-    document.getElementsByClassName("lab-selection-container")[0].style.display = "block";
-    document.getElementsByClassName("lab-session-container")[0].style.display = "none";
-    document.getElementById('labs-loader').style.display = "block";
+  console.log("displaySelectionView: ");
+  hideEndSessionView();
+  document.getElementsByClassName("intro-container")[0].style.display = "none";
+  document.getElementsByClassName("content")[0].style.display = "block";
+  document.getElementsByClassName("lab-selection-container")[0].style.display = "block";
+  document.getElementsByClassName("lab-session-container")[0].style.display = "none";
+  document.getElementById("labs-loader").style.display = "block";
 
-
-
-    requestRecursive('https://api.github.com/repos/ceduliocezar/shortcutlab/contents/labs/', displayLabsForSelection);
+  loadLabsFromAPI(labsAPIURL);
 }
 
 function removeAllLabsFromList() {
-    var labList = document.getElementById("lab-list-container");
+  var labList = document.getElementById("lab-list-container");
 
-    while (labList.hasChildNodes()) {
-        labList.removeChild(labList.lastChild);
-    }
+  while (labList.hasChildNodes()) {
+    labList.removeChild(labList.lastChild);
+  }
 }
 
 function displayLabSessionView() {
-    console.log('displayLabSessionView: ');
-    document.getElementsByClassName("lab-session-container")[0].style.display = "block";
-    document.getElementsByClassName("intro-container")[0].style.display = "none";
-    document.getElementsByClassName("content")[0].style.display = "block";
-    document.getElementsByClassName("lab-selection-container")[0].style.display = "none";
+  console.log("displayLabSessionView: ");
+  document.getElementsByClassName("lab-session-container")[0].style.display = "block";
+  document.getElementsByClassName("content")[0].style.display = "block";
+  hideLoadingContainer();
+  hideEndSessionView();
+  document.getElementsByClassName("intro-container")[0].style.display = "none";
+  document.getElementsByClassName("lab-selection-container")[0].style.display = "none";
 }
 
-
 function displayLoadingContainer() {
-    console.log('displayLoadingContainer: ');
-    document.getElementsByClassName("lab-loading-container")[0].style.display = "block";
+  console.log("displayLoadingContainer: ");
+  document.getElementsByClassName("lab-loading-container")[0].style.display = "block";
 }
 
 function hideLoadingContainer() {
-    console.log('hideLoadingContainer: ');
-    document.getElementsByClassName("lab-loading-container")[0].style.display = "none";
+  console.log("hideLoadingContainer: ");
+  document.getElementsByClassName("lab-loading-container")[0].style.display = "none";
 }
 
 function hideSelectionView() {
-    console.log("hideSelectionView");
-    document.getElementsByClassName("lab-loading-container")[0].style.display = "none";
+  console.log("hideSelectionView");
+  document.getElementsByClassName("lab-loading-container")[0].style.display = "none";
 }
 
 function displayLabsForSelection(data) {
-    removeAllLabsFromList();
-    console.log('displayLabsForSelection: ' + JSON.stringify(data));
+  removeAllLabsFromList();
+  console.log("displayLabsForSelection: " + JSON.stringify(data));
 
-    var labList = document.getElementById("lab-list-container");
+  var labList = document.getElementById("lab-list-container");
 
-    for (const labKey in data) {
-        if (data.hasOwnProperty(labKey)) {
-            const lab = data[labKey];
+  for (const labKey in data) {
+    if (data.hasOwnProperty(labKey)) {
+      const lab = data[labKey];
 
-            let liElement = document.createElement("li");
-            let aElement = document.createElement("a");
-            aElement.classList.add("list-description");
-            aElement.innerHTML = lab.name;
-            aElement.setAttribute('href', '#');
-            aElement.setAttribute('onclick', "loadLab('" + lab.url + "');return false;");
-            liElement.appendChild(aElement);
-            labList.appendChild(liElement);
-        }
+      let liElement = document.createElement("li");
+      let aElement = document.createElement("a");
+      aElement.classList.add("list-description");
+      aElement.innerHTML = lab.name;
+      aElement.setAttribute("href", "#");
+      aElement.setAttribute(
+        "onclick",
+        "loadLab('" + lab.url + "');return false;"
+      );
+      liElement.appendChild(aElement);
+      labList.appendChild(liElement);
     }
+  }
 
-    document.getElementById('labs-loader').style.display = "none";
+  document.getElementById("labs-loader").style.display = "none";
+}
+
+function hideElement(className) {
+  document.getElementById(className).style.display = "none";
+}
+
+function showElement(className) {
+  document.getElementById(className).style.display = "block";
 }
 
 function openFileSelector() {
-    console.log("openFileSelector");
-    var input = document.createElement("input");
+  console.log("openFileSelector");
+  var input = document.createElement("input");
 
-    input.type = "file";
-    input.accept = ".lab";
-    input.addEventListener("change", function (event) {
-        readFile(event.target.files[0]);
-    });
+  input.type = "file";
+  input.accept = ".lab";
+  input.addEventListener("change", function (event) {
+    readFile(event.target.files[0]);
+  });
 
-    input.click();
+  input.click();
 }
 
 function readFile(file) {
-    console.log("readFile");
-    var reader = new FileReader();
-    reader.onload = function (event) {
-        // The file's text will be printed here
-        var fileLab = JSON.parse(event.target.result);
-        onLoadLab(fileLab);
-    };
+  console.log("readFile");
+  var reader = new FileReader();
+  reader.onload = function (event) {
+    var fileLab = JSON.parse(event.target.result);
+    onLoadLab(fileLab);
+  };
 
-    reader.readAsText(file);
+  reader.readAsText(file);
+}
+
+function startLabSession(lab) {
+  clearLabSessionState();
+  registerKeyListeners();
+
+  displayLabSessionView();
+  selectedLab = lab;
+  currentShortcutIndex = 0;
+  displayShortcut(selectedLab.shortcuts[currentShortcutIndex]);
+}
+
+function hideEndSessionView() {
+  console.log("hideEndSessionView");
+  document.getElementsByClassName("end-session-container")[0].style.display = "none";
+}
+
+function registerKeyListeners() {
+  registerKeyDownListener();
+  registerKeyUpListener();
+}
+
+function unregisterKeyListeners() {
+  window.removeEventListener("keyup", onKeyUp);
+  window.removeEventListener("keydown", onKeyDown);
+}
+
+function registerKeyUpListener() {
+  window.addEventListener("keyup", onKeyUp);
+}
+
+function onKeyUp(event) {
+
+  if (hasKeysPressed()) {
+    matchCombination();
+    resetKeysPressed();
+    displayNextShortcut();
+  }
+
+  event.preventDefault();
+}
+
+function resetKeysPressed() {
+  keysPressed = [];
+  printKeysPressedState();
+}
+
+function hasKeysPressed() {
+  return keysPressed.length > 0;
+}
+
+function registerKeyDownListener() {
+  window.addEventListener("keydown", onKeyDown);
+}
+
+function onKeyDown(event) {
+  const key = event.key;
+
+  if (!keysPressed.includes(key)) {
+    console.log("New key down identified:");
+    keysPressed.push(key);
+    printKeysPressedState();
+  }
+  event.preventDefault();
+}
+
+function retryLab() {
+  startLabSession(selectedLab);
+}
+
+function finishSession() {
+  displaySelectionView();
+}
+
+function clearShortcutHistory() {
+  const historyElement = document.getElementsByClassName("shortcut-list")[0];
+
+  while (historyElement.firstChild) {
+    historyElement.removeChild(historyElement.firstChild);
+  }
+}
+
+function clearLabSessionState() {
+  totalCorrectAnswers = 0;
+  clearShortcutHistory();
+}
+
+function clearKeysPressedWhenEnterOnBackgroundState() {
+
+  window.addEventListener('blur', function () {
+    clearKeysPressed();
+  }, false);
+
+  window.addEventListener('focus', function () {
+    clearKeysPressed();
+  }, false);
+}
+
+function clearKeysPressed() {
+  keysPressed = [];
+  printKeysPressedState();
 }
 
